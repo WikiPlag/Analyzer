87c10ab765b92130f6e709ce46343eed954dae38
Julian Krieft <s0548838@htw-berlin.de>
2016-11-07 18:23:24 +0100

    removed apply from main, PlagiarismFinder

--- src/main/scala/de/htw/ai/wikiplag/textProcessing/Main.scala
+++ src/main/scala/de/htw/ai/wikiplag/textProcessing/Main.scala
@@ -4,6 +4,6 @@ import de.htw.ai.wikiplag.textProcessing.plagiarism.PlagiarismFinder
 
 object Main {
   def main(args: Array[String]): Unit = {
-    PlagiarismFinder.apply("das ist ein plagiat")
+    PlagiarismFinder("das ist ein plagiat")
   }
 }
